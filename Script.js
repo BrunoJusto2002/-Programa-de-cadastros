74b1bfcf31dc2c0fe22d933c2dc2840c3e2de76d
Bruno Justo <85250651+BrunoJusto2002@users.noreply.github.com>
2021-10-03 17:30:22 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,8 @@
+function Enviar(){
+    // função alerta obrigado por enviar os seus dados
+    var nome = document.getElementById("Nomeid");
+
+    if (nome.value != ""){
+        alert("Obrigada " +  Nomeid.value  + " por enviar os dados");
+    }
+}
